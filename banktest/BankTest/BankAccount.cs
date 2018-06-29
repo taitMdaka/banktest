180be7067e4606b267d8fb7771f6c01601ccaaae
Tait Mdaka <Tait.Mdaka@avisbudget.co.za>
2018-06-29 13:11:55 +0200

    test complete

---
+++ banktest/BankTest/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTest
+{
+    public class BankAccount
+    {
+        string _customerName;
+        double _balance;
+        public BankAccount(string customerName, double balance)
+        {
+            _customerName = customerName;
+            _balance = balance;
+        }
+
+            public double Balace { get { return _balance; } }
+
+        public void Debit(double amount)
+        {
+            if (_balance == 0)
+                throw new Exception("Balance = 0");
+            if (amount <= 0 || amount > _balance) 
+            throw new Exception("the amount is <=0 or ammount > balance ");
+            _balance -= amount; 
+        }
+        public void Credit (double amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("amount <= 0");
+            _balance += amount; 
+
+        }
+    }
+}
+
---
+++ banktest/BankTestTests/BankAccountTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTest.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        public void TestExceptionCase()
+        {
+            BankAccount bank = new BankAccount("Tait", 0);
+            try
+            {
+                bank.Debit(2);
+            }
+            catch (ArgumentException e)
+            {
+
+                StringAssert.Contains(e.Message, "amount <= 0 orngfd,m");
+                return;
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "Balance = 0 ");
+                return;
+            }
+            Assert.Fail("No exception was thrown");
+        }
+        [TestMethod()]
+        public void TestCreditCase()
+        {
+            BankAccount bank = new BankAccount("yyy", 2);
+            bank.Debit(2);
+            Assert.AreEqual(2, bank.Balace);
+ 
+         }
+        [TestMethod()]
+        public void TestCreditDebitCase()
+        {
+            BankAccount bank = new BankAccount("yyy", 2);
+            bank.Debit(2);
+            bank.Credit(2);
+            Assert.AreEqual(2, bank.Balace);
+
+        }
+
+    }
+}
